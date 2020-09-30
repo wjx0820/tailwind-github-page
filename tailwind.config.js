99f0604125fe7ef065f2d90f3804904222c21920
Jexxie <1989wjx@gmail.com>
2020-09-30 16:43:49 +0800

    file explorer and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
   },
   purge: [],
   theme: {
+    typography: (theme) => ({
+      default: {
+        css: {
+          a: {
+            color: theme('colors.blue.600'),
+          },
+          img: {
+            display: 'inline-block'
+          },
+          hr: {
+            borderColor: theme('colors.gray.400'),
+            marginTop: '2em',
+            marginBottom: '2em',
+          }
+        }
+      }
+    }),
     extend: {
       colors: {
         'gray-700-github': '#404448',
@@ -14,9 +31,23 @@ module.exports = {
         '22': '5.5rem',
         '72': '18rem',
         '200': '50rem',
+      },
+      fontFamily: {
+        mono: [
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
       }
     },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
