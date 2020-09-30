ca17463d31a9423bd8ff7a5436524d3afa480b95
Jexxie <1989wjx@gmail.com>
2020-09-30 12:03:51 +0800

    top nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-700-github': '#404448',
+      },
+      spacing: {
+        '14': '3.5rem',
+        '22': '5.5rem',
+        '72': '18rem',
+        '200': '50rem',
+      }
+    },
   },
   variants: {},
   plugins: [],
